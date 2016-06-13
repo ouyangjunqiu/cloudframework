eb30e52dd90eee13043eb5e63406c1124004e55a
oShine <oyjqdlp@126.com>
2016-06-13 15:10:28 +0800

    修正目录结构
    修正目录结构

--- library/toplinker/lib/TopLinker/Request/SimbaCustomersAuthorizedGet.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class TopLinker_Request_SimbaCustomersAuthorizedGet extends TopLinker_Request_Abstract
-{
-}
--- library/toplinker/lib/TopLinker/Response/Common.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class TopLinker_Response_Common extends TopLinker_Response_Abstract
-{
-}
