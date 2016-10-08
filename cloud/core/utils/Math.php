9528772da0230a8a4132df48e94f2873ebde8916
oShine <oyjqdlp@126.com>
2016-10-08 15:44:35 +0800

    v1.0.4

--- cloud/core/utils/Math.php
+++ cloud/core/utils/Math.php
@@ -36,4 +36,44 @@ class Math
         return number_format($number,2);
     }
 
+    /**
+     * 最小二乘法（又称最小平方法）是一种数学优化技术。它通过最小化误差的平方和寻找数据的最佳函数匹配。
+     * @param array $plot 二维数据 x1=>y1,x2=>y2
+     * @return float|int
+     */
+    public static function polyfitEx($plot = array()){
+        if(empty($plot)){
+            return 0;
+        }
+
+        $x = array_keys($plot);
+        $y = array_values($plot);
+
+        $a = self::polyfit($x,$y);
+        return $a;
+    }
+
+    /**
+     * 最小二乘法（又称最小平方法）是一种数学优化技术。它通过最小化误差的平方和寻找数据的最佳函数匹配。
+     * @param array $x
+     * @param array $y
+     * @return float
+     */
+    public static function polyfit($x = array(),$y = array()){
+
+        $n = count($x);
+        $Exy = 0;
+        $Ex2 = 0;
+        $Ex = 0;
+        $Ey = 0;
+        for($i=0;$i<$n;$i++){
+            $Exy += $x[$i]*$y[$i];
+            $Ex2 += $x[$i]*$x[$i];
+            $Ex += $x[$i];
+            $Ey += $y[$i];
+        }
+        $a = ($n*$Exy-$Ex*$Ey)/($n*$Ex2-$Ex*$Ex);
+        return $a;
+    }
+
 }
