08cf9de1f1a7fb36066d9265cfb902ea8f4f7cb9
oShine <oyjqdlp@126.com>
2016-10-14 10:56:40 +0800

    v1.0.5

--- cloud/core/modules/Module.php
+++ cloud/core/modules/Module.php
@@ -11,7 +11,7 @@ class Module extends CWebModule {
      * 模块初始化方法，重写控制器的命名空间，2执行所有模块注册的初始化行为
      */
     final protected function init() {
-        $this->controllerNamespace = 'application\modules\\' . $this->__MODULE__ . '\controllers';
+        $this->controllerNamespace = 'application\modules\\' . $this->getId() . '\controllers';
         parent::init();
     }
 	
